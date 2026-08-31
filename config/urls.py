3280629dc212e8f961f41fa55f74d440c933948d
Jadj <154758578+Jadj22@users.noreply.github.com>
2026-08-31 12:08:46 +0000

    Update URL routing to include root path for accounts app
    - Added a new route in `urls.py` to include the accounts app at the root path, allowing direct access to its endpoints.

--- config/urls.py
+++ config/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path(".well-known/assetlinks.json", assetlinks),
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
+    path("", include("accounts.urls")),
     path(
         "api/schema/",
         SpectacularAPIView.as_view(),
